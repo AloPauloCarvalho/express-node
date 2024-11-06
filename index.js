4a6ff66dc8757eed069e3d73bb86fa54f0bd0f75
Paulo Carvalho <142057167+AloPauloCarvalho@users.noreply.github.com>
2024-11-06 10:42:18 -0300

    Modificações + DB criado

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + 'index.html');
 });
 
+app.get('/note/:id', (req, res) => {
+    res.sendFile(__dirname, + '/public/note.html')
+});
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
